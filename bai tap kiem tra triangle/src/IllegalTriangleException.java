9bfbf7cbcc9e499a401f78cb67a546b47f05b390
Dinh Phu <ndp17hp@gmail.com>
2020-05-12 17:32:07 +0700

    them file bai tap

---
+++ bai tap kiem tra triangle/src/IllegalTriangleException.java
@@ -0,0 +1,8 @@
+public class IllegalTriangleException extends Exception{
+    public IllegalTriangleException(){
+
+    }
+    public IllegalTriangleException(String gripe){
+        super(gripe);
+    }
+}
---
+++ bai tap kiem tra triangle/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IllegalTriangleException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Nhập cạnh a: ");
+        double a= in.nextDouble();
+        System.out.print("Nhập cạnh b: ");
+        double b= in.nextDouble();
+        System.out.print("Nhập cạnh c: ");
+        double c= in.nextDouble();
+
+        if(isNegative(a) || isNegative(b) || isNegative(c) ){
+            throw new IllegalTriangleException("Your side can't be a negative number");
+        }else if(is2SideLessThan1Side(a,b,c)){
+            throw new IllegalTriangleException("Total of 2 side is less than 1 side");
+        }else{
+            System.out.println("Here is a triangle");
+        }
+
+        
+
+    }
+
+    public static boolean isNegative(double inputNumber){
+        if (inputNumber < 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static  boolean is2SideLessThan1Side(double a,double b,double c){
+        if ( (a + b < c) || (a + c < b) || (c + b < a))
+            return true;
+        else return false;
+    }
+}
